7e9389e58ec2ef44459319617cfa320c315d6823
SiTaggart <me@simontaggart.com>
2023-05-03 19:14:00 -0700

    start boiler plate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import { Box } from '@twilio-paste/core/box';
+import { Theme } from '@twilio-paste/core/theme';
+import { Heading } from '@twilio-paste/core/heading';
+import { Paragraph } from '@twilio-paste/core/paragraph';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <Theme.Provider theme="twilio">
+      <Box>
+        <Heading as="h1" variant="heading10">
+          Hello StackBlitz!
+        </Heading>
+        <Paragraph>Start editing to see some magic happen :)</Paragraph>
+      </Box>
+    </Theme.Provider>
   );
 }
